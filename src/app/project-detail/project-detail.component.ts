195466dcc826d4860f1ed9ffd1289f3a40ba17cc
shy <shyamal.punekar@gmail.com>
2017-10-01 09:54:14 -0700

    Add services to display details

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -2,21 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
+
 
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
-  styleUrls: ['./project-detail.component.css']
+  styleUrls: ['./project-detail.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectDetailComponent implements OnInit {
-   projectId: number = null;
-   constructor(private route: ActivatedRoute, private location: Location) {}
+   projectId: number;
+   projectToDisplay: Project;
+   constructor(private route: ActivatedRoute, private location: Location, private projectService: ProjectService) {}
 
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.projectId = parseInt(urlParameters['id']);
     });
+    this.projectToDisplay = this.projectService.getProjectById(this.projectId);
   }
 
 }
--- src/app/projectplace/projectplace.component.ts
+++ src/app/projectplace/projectplace.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../project.model';
 import { Router } from '@angular/router';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-projectplace',
   templateUrl: './projectplace.component.html',
-  styleUrls: ['./projectplace.component.css']
+  styleUrls: ['./projectplace.component.css'],
+  providers: [ProjectService]
 })
-export class ProjectplaceComponent {
-  projects: Project[] = [
-    new Project('Galactic Age Calculator', 'HTML/CSS,JavaScript/ES6', 'calculates age based on planer', 1),
-    new Project('Team Tracker', 'HTML/CSS, Java, RESTFUL Webservices', 'trackes team member details using CRUD', 2),
-    new Project('Better Doctor API', 'testthree', '', 3),
-    new Project('WildLife Safari', '', '', 4)
-  ];
+export class ProjectplaceComponent implements OnInit{
+  projects: Project[];
 
-  constructor(private router: Router ){};
+  // projects: Project[] = [
+  //   new Project('Galactic Age Calculator', 'HTML/CSS,JavaScript/ES6', 'calculates age based on planer', 1),
+  //   new Project('Team Tracker', 'HTML/CSS, Java, RESTFUL Webservices', 'trackes team member details using CRUD', 2),
+  //   new Project('Better Doctor API', 'testthree', '', 3),
+  //   new Project('WildLife Safari', '', '', 4)
+  // ];
+
+  constructor(private router: Router, private projectService: ProjectService ){};
+
+  ngOnInit(){
+    this.projects = this.projectService.getProjects(); 
+  }
 
   goToDetailPage(clickedProject: Project) {
      this.router.navigate(['projects', clickedProject.id]);
